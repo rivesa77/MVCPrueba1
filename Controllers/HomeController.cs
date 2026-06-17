ba9c6e78e7eedf50033620445a3f7400d278a9aa
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-06-17 11:33:15 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+namespace MVCPrueba1.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using MVCPrueba1.Models;
+    using System.Diagnostics;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
