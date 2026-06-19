6aec3e211bf201735a5ccd8a321a103ef4b74245
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-06-19 11:41:33 +0200

    Creado PersonCollectionViewModel

---
+++ Models/PersonCollectionViewModel.cs
@@ -0,0 +1,11 @@
+﻿// <copyright file="PersonCollectionViewModel.cs" company="Ricardo">
+// Copyright (c) Ricardo. All rights reserved.
+// </copyright>
+
+namespace MVCPrueba1.Models
+{
+    public class PersonCollectionViewModel
+    {
+        public IEnumerable<PersonViewModel> Persons { get; set; }
+    }
+}
