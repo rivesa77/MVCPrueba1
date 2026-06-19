84468aeee57243c655effac677cb469bdfa44c89
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-06-19 20:43:53 +0200

    Ajustada migracion

---
+++ src/MVCPrueba1.Infrastructure/Data/Migrations/20260619184014_CleanArchitectureModelSync.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCPrueba1.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CleanArchitectureModelSync : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Persons_AspNetUsers_UserId",
+                table: "Persons");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Persons",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DNI",
+                table: "Persons",
+                type: "nvarchar(9)",
+                maxLength: 9,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Persons_AspNetUsers_UserId",
+                table: "Persons",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Persons_AspNetUsers_UserId",
+                table: "Persons");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Persons",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DNI",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(9)",
+                oldMaxLength: 9);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Persons_AspNetUsers_UserId",
+                table: "Persons",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
